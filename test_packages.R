48fbadba88b4346360293284af0e4124c74bb8ea
GabrielG228 <gabriel.grosu17@gmail.com>
2023-09-20 08:53:31 +0000

    conflict 2

--- test_packages.R
+++ test_packages.R
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 library(RPostgres)
-library(tidyverse) #Loads many packages
-=======
-library(RPostgres)
-library(tidyverse) #Often used in data science
->>>>>>> 19dc603d57d735567c8650c18fa1532d511e3089
+library(tidyverse) #Loads many packages and Often used in data science
